ca1dfaadbe7dd720e1ad8f14e9394c54f0824a75
MrUse77 <agusdor14@hotmail.com>
2025-02-02 18:26:53 -0300

    Refactor la ruta GET para manejar la ausencia de shortId y mejorar la gestión de errores

--- src/pages/[shortId].ts
+++ src/pages/[shortId].ts
@@ -4,24 +4,27 @@ import { shortenerURL } from "./api/shortener";
 export const GET: APIRoute = async ({ params, redirect }) => {
   await connectDB();
   const { shortId } = params;
-  console.log({ shortId });
-  try {
-    const response = await shortenerURL.findOne({ shortUrl: shortId });
-    let url = response.url;
-    if (response) {
-      if (
-        !response.url.startsWith("http") ||
-        !response.url.startsWith("https")
-      ) {
-        url = "https://" + url;
-      }
+  if (shortId) {
+    try {
+      const response = await shortenerURL.findOne({ shortUrl: shortId });
+      let url = response.url;
+      if (response) {
+        if (
+          !response.url.startsWith("http") ||
+          !response.url.startsWith("https")
+        ) {
+          url = "https://" + url;
+        }
 
-      return redirect(url);
-    } else {
-      return new Response("Not found", { status: 404 });
+        return redirect(url);
+      } else {
+        return new Response("Not found", { status: 404 });
+      }
+    } catch (err) {
+      console.log(err);
+      return new Response(null, { status: 500 });
     }
-  } catch (err) {
-    console.log(err);
-    return new Response(null, { status: 500 });
+  } else {
+    return new Response("Not found", { status: 404 });
   }
 };
